344d0478ceaf27b2ddc69f1dcf55217838ffc3f5
Brad <bradleyfrizzell@gmail.com>
2016-04-19 17:32:06 -0400

    Implemented switching between views

--- Reporter/index.ios.js
+++ Reporter/index.ios.js
@@ -11,51 +11,39 @@ import React, {
   StyleSheet,
   Text,
   TabBarIOS,
+  NavigatorIOS,
   View,
 } from 'react-native';
 
 var Table = require('./table.ios');
-var NewIncident = require('./new_incident.ios');
-var ViewIncident= require('./view_incident.ios');
 
 class Reporter extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      selectedTab: 'table'
-    };
   }
 
   render() {
     return (
-      <TabBarIOS selectedTab={this.state.selectedTab}>
-        <TabBarIOS.Item
-          selected={this.state.selectedTab === 'table'}
-          systemIcon="featured"
-          onPress={() => {
-              this.setState({
-                  selectedTab: 'table',
-              });
-          }}>
-            <Table/>
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          selected={this.state.selectedTab === 'new_incident'}
-          systemIcon="featured"
-          onPress={() => {
-                this.setState({
-                    selectedTab: 'new_incident',
-                });
-          }}>
-          <NewIncident/>
-        </TabBarIOS.Item>
-      </TabBarIOS>
+      <NavigatorIOS
+      style={styles.container}
+      translucent={false}
+      initialRoute={{
+        title: 'Table',
+        component: Table,
+      }}
+      >
+
+      </NavigatorIOS>
     );
   }
 
 }
 
 var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
 });
 
 AppRegistry.registerComponent('Reporter', () => Reporter);
